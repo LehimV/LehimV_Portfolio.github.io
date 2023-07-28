98af08b486eacc615879731b6f44eae6645fa945
LehimV <lehim.vergara@gmail.com>
2023-07-28 08:34:48 -0600

    dark-light mode

--- js/app.js
+++ js/app.js
@@ -1,10 +1,20 @@
 const html = document.querySelector("html");
-console.log(html.dataset);
 const darkBtn = document.querySelector("#btn-darkMode");
-const darkBtn2 = document.querySelector("#btn2-darkMode");
+const darkModeIcon = document.querySelector("#darkModeIcon");
+
 darkBtn.addEventListener("click", () => {
   html.dataset.bsTheme = html.dataset.bsTheme == "dark" ? "light" : "dark";
+  updateDarkModeIcon();
 });
-darkBtn2.addEventListener("click", () => {
-  html.dataset.bsTheme = html.dataset.bsTheme == "dark" ? "light" : "dark";
-});
+
+function updateDarkModeIcon() {
+  const currentTheme = html.dataset.bsTheme;
+  if (currentTheme === "dark") {
+    darkModeIcon.textContent = "light_mode";
+    darkModeText.textContent = "Light Mode";
+  } else {
+    darkModeIcon.textContent = "dark_mode";
+    darkModeText.textContent = "Dark Mode";
+  }
+}
+updateDarkModeIcon();
